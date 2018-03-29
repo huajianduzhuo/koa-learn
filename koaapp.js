cbfaaf21e7e73652b0ecfa01b744f52dc6eac347
yujingm <yujing.meng@capgemini.com>
2018-03-29 11:21:38 +0800

    create koa server

---
+++ koaapp.js
@@ -0,0 +1,26 @@
+const Koa = require('koa')
+const app = new Koa()
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  ctx.set('X-Response-Time', `${ms}ms`)
+})
+
+// logger
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`)
+})
+
+// response
+app.use(ctx => {
+  console.log(ctx)
+  ctx.body = 'Hello World2'
+})
+
+app.listen(3001)
