68789872ead8b118cb58ed65ac3204326221ac2a
huajianduzhuo <yujing_m4370@163.com>
2019-03-18 11:25:44 +0800

    自动获取静态页面，添加测试aria

--- demos/05-koa-static.js
+++ demos/05-koa-static.js
@@ -1,9 +1,22 @@
 const Koa = require('koa')
 const path = require('path')
 const serve = require('koa-static')
+const router = require('koa-router')()
+const fs = require('fs')
 const app = new Koa()
 
-const main = serve(path.join(__dirname, '../public'))
+const main = serve(path.join(__dirname, '../src/views'))
 app.use(main)
+
+router.get('/getFileList', async (ctx, next) => {
+  let files = await fs.readdirSync(path.join(__dirname, '../src/views'))
+  files = files.filter(filename => filename.endsWith('.html') && filename !== 'index.html').map(filename => filename.replace(/\.html/, ''))
+  ctx.type = 'json'
+  ctx.body = {code: 0, files}
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(3000)
